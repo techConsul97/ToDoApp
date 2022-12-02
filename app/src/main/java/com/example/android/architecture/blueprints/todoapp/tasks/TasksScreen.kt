60cabd9c8c7eadffa9aafc192d97205bdc5a3b70
techConsul97 <ittechconsultantemail@gmail.com>
2022-12-02 12:33:57 +0000

    Added search functionality

---
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksScreen.kt
@@ -0,0 +1,377 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.architecture.blueprints.todoapp.tasks
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.tasks.TasksFilterType.*
+import com.example.android.architecture.blueprints.todoapp.util.Async
+import com.example.android.architecture.blueprints.todoapp.util.LoadingContent
+import com.example.android.architecture.blueprints.todoapp.util.TasksTopAppBar
+import com.google.accompanist.appcompattheme.AppCompatTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.coroutines.coroutineContext
+
+@OptIn(ExperimentalLifecycleComposeApi::class)
+@Composable
+fun TasksScreen(
+    @StringRes userMessage: Int,
+    onAddTask: () -> Unit,
+    onTaskClick: (Task) -> Unit,
+    onUserMessageDisplayed: () -> Unit,
+    openDrawer: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: TasksViewModel = hiltViewModel(),
+    scaffoldState: ScaffoldState = rememberScaffoldState()
+) {
+    var isSearchToggled by remember {
+        mutableStateOf(false)
+    }
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            TasksTopAppBar(
+                openDrawer = openDrawer,
+                onFilterAllTasks = { viewModel.setFiltering(ALL_TASKS) },
+                onFilterActiveTasks = { viewModel.setFiltering(ACTIVE_TASKS) },
+                onFilterCompletedTasks = { viewModel.setFiltering(COMPLETED_TASKS) },
+                onClearCompletedTasks = { viewModel.clearCompletedTasks() },
+                onRefresh = { viewModel.refresh() }
+            )
+        },
+        modifier = modifier.fillMaxSize(),
+        floatingActionButton = {
+            FloatingActionButton(onClick = onAddTask) {
+                Icon(Icons.Filled.Add, stringResource(id = R.string.add_task))
+            }
+        }
+    ) { paddingValues ->
+        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+        TasksContent(
+            loading = uiState.isLoading,
+            tasks = uiState.items,
+            currentFilteringLabel = uiState.filteringUiInfo.currentFilteringLabel,
+            noTasksLabel = uiState.filteringUiInfo.noTasksLabel,
+            noTasksIconRes = uiState.filteringUiInfo.noTaskIconRes,
+            onRefresh = viewModel::refresh,
+            onTaskClick = onTaskClick,
+            onTaskCheckedChange = viewModel::completeTask,
+            modifier = Modifier.padding(paddingValues),
+            toggleSearchbar = isSearchToggled
+        )
+
+        // Check for user messages to display on the screen
+        uiState.userMessage?.let { message ->
+            val snackbarText = stringResource(message)
+            LaunchedEffect(scaffoldState, viewModel, message, snackbarText) {
+                scaffoldState.snackbarHostState.showSnackbar(snackbarText)
+                viewModel.snackbarMessageShown()
+            }
+        }
+
+        // Check if there's a userMessage to show to the user
+        val currentOnUserMessageDisplayed by rememberUpdatedState(onUserMessageDisplayed)
+        LaunchedEffect(userMessage) {
+            if (userMessage != 0) {
+                viewModel.showEditResultMessage(userMessage)
+                currentOnUserMessageDisplayed()
+            }
+        }
+    }
+}
+
+@Composable
+private fun TasksContent(
+    viewModel: TasksViewModel = hiltViewModel(),
+    toggleSearchbar: Boolean = false,
+    loading: Boolean,
+    tasks: List<Task>,
+    @StringRes currentFilteringLabel: Int,
+    @StringRes noTasksLabel: Int,
+    @DrawableRes noTasksIconRes: Int,
+    onRefresh: () -> Unit,
+    onTaskClick: (Task) -> Unit,
+    onTaskCheckedChange: (Task, Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val searchedTask by viewModel._searchedTasks.collectAsState(initial = Async.Loading)
+    LoadingContent(
+        loading = loading,
+        empty = tasks.isEmpty() && !loading,
+        emptyContent = { TasksEmptyContent(noTasksLabel, noTasksIconRes, modifier) },
+        onRefresh = onRefresh
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(horizontal = dimensionResource(id = R.dimen.horizontal_margin))
+        ) {
+            Text(
+                text = stringResource(currentFilteringLabel),
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.list_item_padding),
+                    vertical = dimensionResource(id = R.dimen.vertical_margin)
+                ),
+                style = MaterialTheme.typography.h6
+            )
+            if (!toggleSearchbar) { //needs modificaton
+                SearchBar(hint = "Search..", modifier = Modifier.fillMaxWidth()){
+                    viewModel.getSearchedTasksUsingWord(it)
+                }
+            }
+            LazyColumn {
+                if(searchedTask is Async.Success){
+                    items((searchedTask as Async.Success<List<Task>>).data){ task ->
+                        TaskItem(
+                            task = task,
+                            onTaskClick = onTaskClick,
+                            onCheckedChange = { onTaskCheckedChange(task, it) }
+                        )
+                    }
+                }
+                else{
+                    items(tasks) { task ->
+                        TaskItem(
+                            task = task,
+                            onTaskClick = onTaskClick,
+                            onCheckedChange = { onTaskCheckedChange(task, it) }
+                        )
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    hint: String = "",
+    onSearch: (String) -> Unit = {}
+) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    var isHintDisplayed by remember {
+        mutableStateOf(hint != "")
+    }
+    Box(modifier = modifier) {
+        BasicTextField(
+            value = text, onValueChange = {
+                text = it
+                onSearch(it)
+            },
+            maxLines = 1,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black),
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(10.dp, CircleShape)
+                .background(Color.White, CircleShape)
+                .border(1.dp, Color.LightGray, CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp)
+                .onFocusChanged { isHintDisplayed = !it.isFocused },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+            keyboardActions = KeyboardActions(onSearch = {
+                onSearch(text)
+            })
+        )
+        if (isHintDisplayed) {
+            Text(
+                text = hint,
+                color = Color.Black,
+                modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp)
+            )
+        }
+
+    }
+}
+
+@Composable
+private fun TaskItem(
+    task: Task,
+    onCheckedChange: (Boolean) -> Unit,
+    onTaskClick: (Task) -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                horizontal = dimensionResource(id = R.dimen.horizontal_margin),
+                vertical = dimensionResource(id = R.dimen.list_item_padding),
+            )
+            .clickable { onTaskClick(task) }
+    ) {
+        Checkbox(
+            checked = task.isCompleted,
+            onCheckedChange = onCheckedChange
+        )
+        Text(
+            text = task.titleForList,
+            style = MaterialTheme.typography.h6,
+            modifier = Modifier.padding(
+                start = dimensionResource(id = R.dimen.horizontal_margin)
+            ),
+            textDecoration = if (task.isCompleted) {
+                TextDecoration.LineThrough
+            } else {
+                null
+            }
+        )
+    }
+}
+
+@Composable
+private fun TasksEmptyContent(
+    @StringRes noTasksLabel: Int,
+    @DrawableRes noTasksIconRes: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = noTasksIconRes),
+            contentDescription = stringResource(R.string.no_tasks_image_content_description),
+            modifier = Modifier.size(96.dp)
+        )
+        Text(stringResource(id = noTasksLabel))
+    }
+}
+
+@Preview
+@Composable
+private fun TasksContentPreview() {
+    AppCompatTheme {
+        Surface {
+            TasksContent(
+                loading = false,
+                tasks = listOf(
+                    Task("Title 1", "Description 1"),
+                    Task("Title 2", "Description 2", true),
+                    Task("Title 3", "Description 3", true),
+                    Task("Title 4", "Description 4"),
+                    Task("Title 5", "Description 5", true)
+                ),
+                currentFilteringLabel = R.string.label_all,
+                noTasksLabel = R.string.no_tasks_all,
+                noTasksIconRes = R.drawable.logo_no_fill,
+                onRefresh = { },
+                onTaskClick = { },
+                onTaskCheckedChange = { _, _ -> },
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TasksContentEmptyPreview() {
+    AppCompatTheme {
+        Surface {
+            TasksContent(
+                loading = false,
+                tasks = emptyList(),
+                currentFilteringLabel = R.string.label_all,
+                noTasksLabel = R.string.no_tasks_all,
+                noTasksIconRes = R.drawable.logo_no_fill,
+                onRefresh = { },
+                onTaskClick = { },
+                onTaskCheckedChange = { _, _ -> },
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TasksEmptyContentPreview() {
+    AppCompatTheme {
+        Surface {
+            TasksEmptyContent(
+                noTasksLabel = R.string.no_tasks_all,
+                noTasksIconRes = R.drawable.logo_no_fill
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TaskItemPreview() {
+    AppCompatTheme {
+        Surface {
+            TaskItem(
+                task = Task("Title", "Description"),
+                onTaskClick = { },
+                onCheckedChange = { }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TaskItemCompletedPreview() {
+    AppCompatTheme {
+        Surface {
+            TaskItem(
+                task = Task("Title", "Description", true),
+                onTaskClick = { },
+                onCheckedChange = { }
+            )
+        }
+    }
+}
